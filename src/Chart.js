abe5203afd272056578654aa84fddf0a8609387d
pavlo-paliychuk-insighty <pavelko1608@gmail.com>
2018-06-13 10:02:58 +0300

    Add print button

--- src/Chart.js
+++ src/Chart.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import d3 from "d3";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 window.d3 = d3;
 
 export default class Chart extends Component {
 state = {r: 1};
+  saveDocument = (elementId) => {
+    const input = document.getElementById(elementId);
+    console.log(input);
+    html2canvas(input).then((canvas) => {
+      const pdf = new jsPDF('', 'mm', [canvas.width, canvas.height]);
+      const imgData = canvas.toDataURL('image/png', 1.0);
+      pdf.addImage(imgData, 'png', 0, 0, canvas.width * .5, canvas.height * 0.8);
+      pdf.save('test.pdf');
+    });
+  };
   displayGraph = () => {
     const chart = document.querySelector("#chart");
 
@@ -32,7 +44,11 @@ state = {r: 1};
           <label>Enter r:    </label>
           <input type="number" onChange={e => e.target.value && this.setState({r: e.target.value})
           } value={this.state.r} />
+          <div id="divToPrint">
           {this.displayGraph()}
+          </div>
+          <button style={{marginRight: 10}} className="button" type="button" onClick={() => this.saveDocument('chart')}>saveDocument</button>
+
         </div>
     );
   }
